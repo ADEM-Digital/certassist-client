b71162f7464b4c214020a423b5aaaa37f0697dea
Wellington Abreu <wellington.abreu@gmail.com>
2023-12-20 18:02:45 -0400

    Implemented Log Out

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,15 +16,12 @@ const sidebarMenu = [
   },
 ];
 
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
+
 
 const Layout = ({}) => {
-  const { user } = useAuth0();
+  const { user, logout } = useAuth0();
 
+  
   const checkUserData = async () => {
     try {
       if (user?.sub) {
@@ -128,21 +125,21 @@ const Layout = ({}) => {
               leaveTo="transform opacity-0 scale-95"
             >
               <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {userNavigation.map((item) => (
-                  <Menu.Item key={item.name}>
-                    {({ active }) => (
-                      <a
-                        href={item.href}
+            
+                  <Menu.Item>
+             
+                      <button
+                        onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
                         className={classNames(
-                          active ? "bg-gray-100" : "",
+                         
                           "block px-4 py-2 text-sm text-gray-700"
                         )}
                       >
-                        {item.name}
-                      </a>
-                    )}
+                        Log Out
+                      </button>
+               
                   </Menu.Item>
-                ))}
+            
               </Menu.Items>
             </Transition>
           </Menu>
